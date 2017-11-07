f562695f681ee939777ab41e0e6f0d4b196bf964
Calipso997 <marekkamzol@tlen.pl>
2017-11-07 14:15:21 +0100

    lenovo

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import './style.scss';
+import $ from 'jquery';
+import moment from 'moment';
+require('font-awesome/css/font-awesome.css');
+
+
+const port = 3000;
+const apiUri = `http://localhost:${port}`;
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const VENDOR_LIBS=[
+];
+
+module.exports = {
+    entry:{
+        bundle: './app/index.js',
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {   test: /\.(ttf|eot|woff|woff2)$/,
+                loader: "url-loader?limit=10000&minetype=application/font-woff"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use:[
+                    {
+                        loader: 'url-loader',
+                        options:{
+                            limit:40000
+                        }
+                    },
+                    {
+                        loader:'image-webpack-loader',
+                        options:{
+                        }
+                    }
+                ]
+            }
+        ],
+        loaders: [
+            // the url-loader uses DataUrls.
+            // the file-loader emits files.
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        ]
+    },
+    devtool: 'source-map',
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'windows.jQuery': 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+            // In case you imported plugins individually, you must also require them here:
+            Util: "exports-loader?Util!bootstrap/js/dist/util",
+            Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+        })
+    ]
+};
+
