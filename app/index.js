0ada62f7800482d67bcadb7cb275d8113ed7a086
Calipso997 <marekkamzol@tlen.pl>
2017-11-08 00:10:01 +0100

    poprawki

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import './style.scss';
+import 'bootstrap';
 import $ from 'jquery';
 import moment from 'moment';
 require('font-awesome/css/font-awesome.css');
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+
 
 
 const port = 3000;
--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,6 @@ module.exports = {
             'windows.jQuery': 'jquery',
             'window.jQuery': 'jquery',
             Popper: ['popper.js', 'default'],
-            // In case you imported plugins individually, you must also require them here:
             Util: "exports-loader?Util!bootstrap/js/dist/util",
             Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
         })
